1c76429d0f189651c726d034ea6625425d53ee05
Minh Nam <duongminhnam99@gmail.com>
2022-01-12 22:59:49 +0700

    middleware for api route

---
+++ httpcore/middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func AdminMiddleware() mux.MiddlewareFunc {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("x-api-key") == os.Getenv("X-API-KEY") {
+				h.ServeHTTP(w, r)
+			}
+			log.Printf("<-- %d %s", 403, http.StatusText(http.StatusForbidden))
+		})
+	}
+}
--- httpcore/route.go
+++ httpcore/route.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/duongnam99/stock-analyzer/httpcore/admin"
 	"github.com/duongnam99/stock-analyzer/httpcore/admin/crudstock"
+	"github.com/duongnam99/stock-analyzer/httpcore/middleware"
 	"github.com/gorilla/mux"
 )
 
 func InitRoutes() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/v1/stocks", crudstock.GetAll).Methods("GET")
-	// r.HandleFunc("/api/stocks/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/v1/stocks", crudstock.Store).Methods("POST")
-	// r.HandleFunc("/api/stocks/{id}", updateBook).Methods("PUT")
-	// r.HandleFunc("/api/v1/stocks/{id}", crudstock.Delete).Methods("DELETE")
-	r.HandleFunc("/api/v1/stocks/{code}", crudstock.DeleteByCode).Methods("DELETE")
+	apiRoute := r.PathPrefix("/api/").Subrouter()
+	apiRoute.HandleFunc("/v1/stocks", crudstock.GetAll).Methods("GET")
+	apiRoute.HandleFunc("/v1/stocks", crudstock.Store).Methods("POST")
+	apiRoute.HandleFunc("/v1/stocks/{code}", crudstock.DeleteByCode).Methods("DELETE")
+	apiRoute.Use(middleware.AdminMiddleware())
 	r.HandleFunc("/stock/report", admin.Report).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":6868", r))
